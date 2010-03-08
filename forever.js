1a97ac2f9b11e365202decbce87e97c696e16b8e
Ryan Berdeen <ryan@ryanberdeen.com>
2010-03-07 22:46:12 -0500

    animate, jump

--- forever.js
+++ forever.js
@@ -47,4 +47,45 @@ for (i = 0; i < data.coords.length; i++) {
   coord.y = (coord.origPos.y -minY) / (maxY - minY);
 }
 
-p.init(p.ajax('pjs/test.pjs'));
+function edgeExists(start, end) {
+    var coord = data.coords[start];
+    if (!coord) {
+        return false;
+    }
+    var edges = coord.outgoing;
+    if (!edges) {
+        return false;
+    }
+    for (var i = 0; i < edges.length; i++) {
+        if (edges[i][1] == end) {
+            return true;
+        }
+    }
+    return false;
+}
+
+for (i = 0; i < data.edges.length; i++) {
+    var edge = data.edges[i];
+    var start = edge[0];
+    var end = edge[1];
+    // is this the first node is a sequence of connected nodes
+    if (!edgeExists(start - 1, end - 1)) {
+        edge.isFirst = true;
+        for (var j = 0; j < 50; j++) {
+            if (!edgeExists(start + j + 1, end + j + 1)) {
+                break;
+            }
+        }
+        edge.sharedLength = j + 1;
+        console.log(edge.sharedLength);
+    }
+    if (!edgeExists(start + 1, end + 1)) {
+        edge.isLast = true;
+    }
+}
+
+var defaultVis = 'test';
+
+var viz = location.hash ? location.hash.substring(1) : defaultVis;
+
+p.init(p.ajax('pjs/' + viz + '.pjs'));
